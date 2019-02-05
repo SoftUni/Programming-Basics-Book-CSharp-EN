8ca29f674ca7032c74e5871ef4665d5b102760a4
Svetlin Nakov <nakov@users.noreply.github.com>
2019-02-05 12:28:01 +0200

    Update video.js

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,12 +1,15 @@
 gitbook.events.bind("page.change", function() {
-    let videos = $(".video-player");
-    for (let video of videos) {
-        let videoText = video.innerText;
-        let regex = /watch\?v=([a-zA-Z0-9_-]+)?/g;
-        let matches = regex.exec(videoText) || [''];
-        let videoId = matches[1];
-        let iframe = "<iframe src='https://www.youtube.com/embed/" + 
-            videoId + "' frameborder='0' allowfullscreen>";
-        $(video).html(iframe);
+	let videoLinks = $("p:contains('video lesson') a, div.video-player a");
+	for (let link of videoLinks) {
+        let regex = /youtube\.com\/watch\?v=([a-zA-Z0-9_-]+).*|youtu\.be\/([a-zA-Z0-9_-]+).*/gi;
+        let matches = regex.exec(link.href);
+		console.log(matches);
+        if (matches) {
+            // A https://www.youtube.com/watch?v=... or https://youtu.be/... link is found --> display the video player
+            let videoId = matches[1] || matches[2];
+            let iframe = "<div class='video-player'><iframe src='https://www.youtube.com/embed/" + 
+                videoId + "' frameborder='0' allowfullscreen></div>";
+            $(link).parent().html(iframe);
+        }
     }
 });
