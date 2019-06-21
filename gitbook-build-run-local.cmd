240c84668c45bcf52a71b33e3d30aa3b3d3d5b44
Svetlin Nakov <github@nakov.com>
2019-06-22 01:22:26 +0300

    Added build and deploy scripts

--- gitbook-build-run-local.cmd
+++ gitbook-build-run-local.cmd
@@ -1,5 +1,12 @@
-gitbook install
-gitbook build .
-gitbook server . --port 4000
+REM npm install -g gitbook-cli
+REM npm install -g serve
+
+call gitbook install
+
+call gitbook build . --log=debug --debug
+
+start serve _book
+
+start "" http://127.0.0.1:5000
 
 pause
